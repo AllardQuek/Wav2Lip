3a666e2d13d7da76aa85a61574002162bdf590d5
Allard <AllardQuek@users.noreply.github.com>
2021-07-24 00:21:14 +0800

    added amazon translate

--- AWS/synthesize_speech.py
+++ AWS/synthesize_speech.py
@@ -3,18 +3,33 @@ Take in some text provided by the user and return an audio file of the speech.
 Example for Chinese text.
 """
 
+
 import boto3
 
 # Credentials can be configured using "aws configure"
 polly_client = boto3.Session(region_name='ap-southeast-1').client('polly')
+translate = boto3.client(service_name='translate',
+                         region_name='ap-south-1')
 
-response = polly_client.synthesize_speech(VoiceId='Raveena',
-                OutputFormat='mp3', 
-				# LanguageCode="cmn-CN",
-                # Text = '你好， 你叫什么名字',
-                Text = 'This is a sample text to be synthesized.',
-                Engine = 'standard')
+text = "Hello world. What's your name?"
 
-file = open('speech.mp3', 'wb')
-file.write(response['AudioStream'].read())
-file.close()
+# Translate text to Chinese
+result = translate.translate_text(Text=text,
+                                  SourceLanguageCode="en",
+                                  TargetLanguageCode="zh")
+print('TranslatedText: ' + result.get('TranslatedText'))
+print('SourceLanguageCode: ' + result.get('SourceLanguageCode'))
+print('TargetLanguageCode: ' + result.get('TargetLanguageCode'))
+
+# Convert text to speech
+response = polly_client.synthesize_speech(VoiceId='Zhiyu',
+                                          OutputFormat='mp3',
+                                          LanguageCode="cmn-CN",
+                                          Text=result.get('TranslatedText'),
+                                          #   Text='你好， 你叫什么名字',
+                                          #   Text='This is a sample text to be synthesized.',
+                                          Engine='standard')
+
+# Save audio output to file
+with open('speech.mp3', 'wb') as file:
+    file.write(response['AudioStream'].read())
